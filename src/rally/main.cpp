f2b073d4e511baaf12de0cbad749e39c34f315be
mpinto70 <mpinto70@076cfda8-7331-363f-0d17-135d5c6ef7e9>
2015-01-09 07:33:07 +0000

    formatting and inclusion of namespaces

--- src/rally/main.cpp
+++ src/rally/main.cpp
@@ -3,18 +3,18 @@
  main code.
  */
 
-#include "CGameLib.h"
- 
+#include "graphic/CGameLib.h"
+
 // Essas duas constantes ficam por aqui mesmo?
 static const unsigned int UTIL_W = 480, UTIL_H = 240;
 
-int main()
-{
-  int iGameLibResult = GameLib::Init(UTIL_W, UTIL_H);
-  iGameLibResult = 0;
+int main() {
+    int iGameLibResult = graphic::GameLib::Init(UTIL_W, UTIL_H);
+    iGameLibResult = 0;
 
-  GameLib::End();
-  
-  return 0;
+    graphic::GameLib::End();
+
+    return 0;
 }
 END_OF_MAIN()
+
