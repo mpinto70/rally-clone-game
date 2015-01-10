2a58e605c3869a09fc2786896edfc66b34d67c8c
mpinto70 <mpinto70@076cfda8-7331-363f-0d17-135d5c6ef7e9>
2015-01-10 08:00:23 +0000

    formatting

--- trunk/src/util/CSingleton.h
+++ trunk/src/util/CSingleton.h
@@ -6,34 +6,34 @@
 namespace util {
 template <typename T>
 class CSingleton {
-public:
-    CSingleton() = delete;
-    ~CSingleton() = delete;
-    CSingleton(const CSingleton &) = delete;
-    CSingleton(CSingleton &&) = delete;
-    CSingleton& operator = (const CSingleton &) = delete;
-    CSingleton& operator = (CSingleton &&) = delete;
+    public:
+        CSingleton() = delete;
+        ~CSingleton() = delete;
+        CSingleton(const CSingleton &) = delete;
+        CSingleton(CSingleton &&) = delete;
+        CSingleton& operator = (const CSingleton &) = delete;
+        CSingleton& operator = (CSingleton &&) = delete;
 
-    template <typename U>
-    static void create(std::unique_ptr<U> u) {
-        if (u.get() == nullptr)
-            throw std::logic_error("CSingleton::create - null pointer");
-        if (instance_.get() != nullptr)
-            throw std::logic_error("CSingleton::create - already created");
-        instance_ = std::move(u);
-    }
+        template <typename U>
+        static void create(std::unique_ptr<U> u) {
+            if (u.get() == nullptr)
+                throw std::logic_error("CSingleton::create - null pointer");
+            if (instance_.get() != nullptr)
+                throw std::logic_error("CSingleton::create - already created");
+            instance_ = std::move(u);
+        }
 
-    static void destroy() {
-        instance_.reset(nullptr);
-    }
+        static void destroy() {
+            instance_.reset(nullptr);
+        }
 
-    static T& instance() {
-        if (instance_.get() == nullptr)
-            throw std::logic_error("CSingleton::instance - not created");
-        return *instance_;
-    }
-private:
-    static std::unique_ptr<T> instance_;
+        static T& instance() {
+            if (instance_.get() == nullptr)
+                throw std::logic_error("CSingleton::instance - not created");
+            return *instance_;
+        }
+    private:
+        static std::unique_ptr<T> instance_;
 };
 
 template <typename T>
