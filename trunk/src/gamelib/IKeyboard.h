5d539fb7b49ca8f6f281f9b1387b0a0f047fd836
killocan@gmail.com <killocan@gmail.com@076cfda8-7331-363f-0d17-135d5c6ef7e9>
2015-01-11 17:37:10 +0000

    - Input do teclado. - Fonte para o menu ripado do jogo - mapa (stage -> map) em imagem.

--- trunk/src/gamelib/IKeyboard.h
+++ trunk/src/gamelib/IKeyboard.h
@@ -5,7 +5,10 @@ namespace gamelib {
 
 class IKeyboard {
     public:
+		enum KEYS {GLIB_KEY_ENTER, GLIB_KEY_LEFT, GLIB_KEY_RIGHT, GLIB_KEY_UP, GLIB_KEY_DOWN, GLIB_KEY_SPACE};
+
         virtual ~IKeyboard() = 0;
+		virtual bool isKeyPressed(KEYS keyCode) = 0;	
 };
 
 }
--- trunk/src/gamelib/allegro/CKeyboard.cpp
+++ trunk/src/gamelib/allegro/CKeyboard.cpp
@@ -9,6 +9,7 @@
 
 namespace gamelib {
 namespace allegro {
+
 CKeyboard::CKeyboard() {
     const int allegResult = install_keyboard();
     if (allegResult != 0)
@@ -16,6 +17,33 @@ CKeyboard::CKeyboard() {
 }
 
 CKeyboard::~CKeyboard() {
+}
+
+bool CKeyboard::isKeyPressed(KEYS keyCode) {
+  unsigned int keyCode = 0;
+
+  switch(keyCode) {
+    case IKeyboard::GLIB_KEY_ENTER:
+	  keyCode = KEY_ENTER;
+	break;
+    case IKeyboard::GLIB_KEY_LEFT:
+	  keyCode = KEY_LEFT;
+	break;
+    case IKeyboard::GLIB_KEY_RIGHT:
+	  keyCode = KEY_RIGHT;
+	break;
+    case IKeyboard::GLIB_KEY_UP:
+	  keyCode = KEY_UP;
+	break;
+    case IKeyboard::GLIB_KEY_DOWN:
+	  keyCode = KEY_DOWN;
+	break;
+    case IKeyboard::GLIB_KEY_SPACE:
+	  keyCode = KEY_SPACE;
+	break;
+  }
+  
+  return (key[keyCode] != 0);
 }
 
 }
