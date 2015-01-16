a31abd23bd2518b935175ae508c3265424c1d63d
mpinto70@gmail.com <mpinto70@gmail.com@076cfda8-7331-363f-0d17-135d5c6ef7e9>
2015-01-16 06:23:13 +0000

    enhancing Graphic class to print

--- trunk/src/gamelib/allegro/CGraphic.cpp
+++ trunk/src/gamelib/allegro/CGraphic.cpp
@@ -5,11 +5,28 @@
 
 #include "CGraphic.h"
 #include "util/CException.h"
+#include <map>
 
 namespace gamelib {
 namespace allegro {
+
+static std::map<COLOR, int> COLORS;
+static void initColor() {
+    COLORS[COLOR::BLACK]    = makecol32(0,   0,   0);
+    COLORS[COLOR::RED]      = makecol32(255, 0,   0);
+    COLORS[COLOR::GREEN]    = makecol32(0,   255, 0);
+    COLORS[COLOR::BLUE]     = makecol32(0,   0,   255);
+    COLORS[COLOR::WHITE]    = makecol32(255, 255, 255);
+}
+
+static int translate(COLOR color) {
+    return COLORS[color];
+}
+
 CGraphic::CGraphic(unsigned int uiWidth, unsigned int uiHeight)
-    : buffer_(nullptr) {
+    : buffer_(nullptr),
+      fontSystem_(nullptr),
+      fontMenu_(nullptr) {
     using util::CException;
 
     set_color_depth(32);
@@ -22,11 +39,37 @@ CGraphic::CGraphic(unsigned int uiWidth, unsigned int uiHeight)
     buffer_ = create_bitmap(SCREEN_W, SCREEN_H);
     if (buffer_ == nullptr)
         throw CException("CGraphic::CGraphic - Error initializing system memory", -1);
+
+    fontMenu_ = load_font(RALLY_ROOT "/Stuff/Menu_font.pcx", nullptr, nullptr);
+    if (fontMenu_ == nullptr)
+        throw CException("CGraphic::CGraphic - Error initializing font subsystem", -2);
+
+    fontSystem_ = load_font(RALLY_ROOT "/Stuff/Menu_font.pcx", nullptr, nullptr);
+    if (fontSystem_ == nullptr)
+        throw CException("CGraphic::CGraphic - Error initializing font subsystem", -3);
+
+    initColor();
 }
 
 CGraphic::~CGraphic() {
     destroy_bitmap(buffer_);
     buffer_ = nullptr;
+}
+
+void CGraphic::printText(const std::string & text,
+                         const GFONT gfont,
+                         const unsigned x,
+                         const unsigned y,
+                         COLOR foreground,
+                         COLOR background) {
+    if (x > width() || y > height())
+        return;
+
+    if (text.empty())
+        return;
+
+    const FONT * font = gfont == GFONT::MENU_FONT ? fontMenu_ : fontSystem_;
+    textprintf_ex(buffer_, font, x, y, translate(foreground), translate(background), "%s", text.c_str());
 }
 
 }
