820fe48c17b924104d2b23782a5c660179122eeb
mpinto70@gmail.com <mpinto70@gmail.com@076cfda8-7331-363f-0d17-135d5c6ef7e9>
2015-01-16 07:33:43 +0000

    formatting

--- trunk/src/gamelib/IFont.h
+++ trunk/src/gamelib/IFont.h
@@ -16,10 +16,10 @@ class IFont {
     public:
         virtual ~IFont() = 0;
         virtual void setFont(GFONT font) = 0;
-		
-		// In order to use struct BITMAP and std::string, I opted for include those header files, how could we manage this?
-		// Create another class that represents a "BITMAP" ?
-		virtual void printText(std::string text, BITMAP * buffer, unsigned x, unsigned y, int foregroundColor, int backgroundColor) = 0;
+
+        // In order to use struct BITMAP and std::string, I opted for include those header files, how could we manage this?
+        // Create another class that represents a "BITMAP" ?
+        virtual void printText(std::string text, BITMAP * buffer, unsigned x, unsigned y, int foregroundColor, int backgroundColor) = 0;
 };
 
 }
