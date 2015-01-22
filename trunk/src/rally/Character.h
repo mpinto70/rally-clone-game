e057eb1edce4e90476476c71b9e6d8bdc8fa78df
mpinto70@gmail.com <mpinto70@gmail.com@076cfda8-7331-363f-0d17-135d5c6ef7e9>
2015-01-22 07:11:38 +0000

    formatting

--- trunk/src/rally/Character.h
+++ trunk/src/rally/Character.h
@@ -4,44 +4,43 @@
 
 #include "animsequence.h"
 
-class Character
-{
-  public:
-    Character(const Stage & stage, unsigned int TYPE);
-    Character();
-    virtual ~Character();
+class Character {
+    public:
+        Character(const Stage & stage, unsigned int TYPE);
+        Character();
+        virtual ~Character();
 
-	// There will be code here, so it's not pure virtual.
-    virtual void doLogic();
+        // There will be code here, so it's not pure virtual.
+        virtual void doLogic();
 
-	// Draw itself to bmp, move to stage?
-    virtual void drawCharacter(BITMAP * bmp);
+        // Draw itself to bmp, move to stage?
+        virtual void drawCharacter(BITMAP * bmp);
 
-	// In case we move draw code to stage, this call will return character current sprite.
-    BITMAP * getFrame();
+        // In case we move draw code to stage, this call will return character current sprite.
+        BITMAP * getFrame();
 
-	// Decides based on curState which animation should be used. Also update animation frame.
-    virtual bool handleAnimation();
+        // Decides based on curState which animation should be used. Also update animation frame.
+        virtual bool handleAnimation();
 
-	// This is only relevant to player, maybe move it from here.
-    virtual void hit(Character * pCharacter);
-	
-	// Reset inicial x,y, inicial animation and such...
-    virtual void respawn();
+        // This is only relevant to player, maybe move it from here.
+        virtual void hit(Character * pCharacter);
 
-    double x, y;
-    double oldX, oldY;
-    double velX, velX;
-	int screenX, screenY;
+        // Reset inicial x,y, inicial animation and such...
+        virtual void respawn();
 
-    int curState;
-    int lastState;
+        double x, y;
+        double oldX, oldY;
+        double velX, velX;
+        int screenX, screenY;
 
-    int colorOffset;
+        int curState;
+        int lastState;
 
-    AnimSequence * spriteSheet;
-	
-	//TODO: In order to implement A* we need to know map geometry and such... move to stage like:
-	// vector<points> path = stage.createPath(point 1, point 2, bool avoid rocks);
-	// ? :)
+        int colorOffset;
+
+        AnimSequence * spriteSheet;
+
+        //TODO: In order to implement A* we need to know map geometry and such... move to stage like:
+        // vector<points> path = stage.createPath(point 1, point 2, bool avoid rocks);
+        // ? :)
 };
