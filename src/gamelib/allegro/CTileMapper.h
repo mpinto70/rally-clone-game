155c1b882d05ac4c299b428fae68b452827f4d09
Marcelo Pinto <mpinto70@gmail.com>
2015-04-02 23:20:14 -0700

    simplifying and securing BITMAP

--- src/gamelib/allegro/CTileMapper.h
+++ src/gamelib/allegro/CTileMapper.h
@@ -4,14 +4,43 @@
 
 #include <allegro.h>
 #include <map>
+#include <memory>
 
 namespace gamelib {
 namespace allegro {
 
+typedef std::unique_ptr<BITMAP, void(*)(BITMAP*)> BITMAP_PTR;
+
+/** Reads the tiles from a file
+ * @param fileName      name of the file with the image
+ * @param tileWidth     size of each tile
+ * @param tileHeight    size of each tile
+ * @param gap           gap between, above and below each tile
+ * @return the full bitmap and the tiles (sub bitmaps)
+ * the expected format of the file is
+\verbatim
+       |------------------------------image width----------------------------------|
+   _ _  ___________________________________________________________________________
+    |  |                                                                           |
+    |  |     g a p       g a p       g a p       g a p       g a p       g a p     |
+    |  |   ,-------,   ,-------,   ,-------,   ,-------,   ,-------,   ,-------,   |---
+ image | g |       | g |       | g |       | g |       | g |       | g |       | g |  |
+height | a | tile  | a | tile  | a | tile  | a | tile  | a | tile  | a | tile  | a |  | tile
+    |  | p |       | p |       | p |       | p |       | p |       | p |       | p |  | height
+    |  |   |       |   |       |   |       |   |       |   |       |   |       |   |  |
+    |  |   `-------´   `-------´   `-------´   `-------´   `-------´   `-------´   |---
+    |  |    g a p        g a p       g a p       g a p       g a p       g a p     |
+   _|_ |___________________________________________________________________________|
+                                                                        |------|
+                                                                          tile
+                                                                          width
+\endverbatim
+ */
 class CTileMapper {
     public:
         CTileMapper(const std::string & fileName,
-                    unsigned tileSize,
+                    unsigned tileWidth,
+                    unsigned tileHeight,
                     unsigned gap);
         ~CTileMapper();
 
@@ -26,8 +55,8 @@ class CTileMapper {
             return tile(tl);
         }
     private:
-        BITMAP * tileSet_;
-        std::map<map::ETile, BITMAP*> tileMap_;
+        BITMAP_PTR fullBitmap_;
+        std::map<map::ETile, BITMAP_PTR> tileMap_;
 };
 }
 }
--- tools/mapeditor/map_editor.cpp
+++ tools/mapeditor/map_editor.cpp
@@ -419,7 +419,7 @@ static void load_tiles(const std::string & dir,
                        unsigned & tiles_num) {
     const std::string tile_name = dir + "/tileset.bmp";
     constexpr unsigned GAP = 2;
-    g_tileMapper.reset(new gamelib::allegro::CTileMapper(tile_name, TILE_SIZE, GAP));
+    g_tileMapper.reset(new gamelib::allegro::CTileMapper(tile_name, TILE_SIZE, TILE_SIZE, GAP));
     constexpr point_t xy = {0, 0};
     constexpr size_t qttyTiles = static_cast<map::tile_t>(map::ETile::LAST);
     g_tiles.xy_pos = std::vector<point_t>(qttyTiles, xy);
