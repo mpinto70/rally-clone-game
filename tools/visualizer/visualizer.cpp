d9a835db3f3acb07c0aeb038c115346f91426910
Marcelo Pinto <mpinto70@gmail.com>
2015-05-08 23:55:35 -0700

    implementing visualization of bot cars and actions

--- tools/visualizer/visualizer.cpp
+++ tools/visualizer/visualizer.cpp
@@ -1,6 +1,7 @@
 
 #include "../tools/util/helpers.h"
 #include "gamelib/allegro/bmp/CCarMapper.h"
+#include "gamelib/allegro/bmp/CActionMapper.h"
 #include "util/CWait.h"
 
 #include <allegro.h>
@@ -54,14 +55,69 @@ static void move_left(unsigned & cur_tile, const unsigned max) {
         cur_tile = max - 1;
 }
 
+template <typename MAPPER>
+void show(MAPPER & mapper,
+                BITMAP * canvas) {
+    util::CWait wait(50);
+    util::CWait miniWait(10);
+    unsigned cur_tile = 0;
+    const int bg = makecol(0xDE, 0x97, 0x47);
+    while (!key[KEY_ESC]) {
+        miniWait.reset();
+        rectfill(canvas, 0, 0, WINDOW_W, WINDOW_H, bg);
+
+        int keyWait = 0;
+        bool shouldWait = false;
+        wait.reset();
+        if (key[KEY_RIGHT]) {
+            move_right(cur_tile, mapper.numBmps());
+            shouldWait = true;
+        } else if (key[KEY_LEFT]) {
+            shouldWait = true;
+            move_left(cur_tile, mapper.numBmps());
+        } else if (key[KEY_D]) {
+            move_right(cur_tile, mapper.numBmps());
+            keyWait = KEY_D;
+        } else if (key[KEY_A]) {
+            move_left(cur_tile, mapper.numBmps());
+            keyWait = KEY_A;
+        }
+
+        textprintf_ex(canvas,
+                      font,
+                      X_IMAGES,
+                      40,
+                      0,
+                      bg,
+                      "number of tiles: %lu / tile size: (%02d x %02d)",
+                      mapper.numBmps(),
+                      mapper[cur_tile]->w,
+                      mapper[cur_tile]->h);
+        draw_full_image(canvas, mapper.fullBmp(), X_IMAGES, 60);
+        draw_tile(canvas, mapper[cur_tile], X_IMAGES, 150);
+
+        vsync();
+        blit(canvas, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
+        clear_bitmap(canvas);
+
+        if (keyWait != 0)
+            tools::hold_while_pressed(keyWait);
+        else if (shouldWait)
+            wait.wait();
+
+        miniWait.wait();
+    }
+}
+
 int main(int argc, char *argv[]) {
     try {
-        if (argc != 3) {
-            exit_visualizer("usage\npath/to/visualizer.exe <path/to/images/file.png> <width of image>");
+        if (argc != 4) {
+            exit_visualizer("usage\npath/to/visualizer.exe <path/to/images/file.png> <width of image> <car|action>");
         }
 
         const std::string file_name = argv[1];
         const unsigned width_of_image = std::stoul(argv[2]);
+        const std::string type = argv[3];
 
         allegro_init();
         install_mouse();
@@ -74,44 +130,12 @@ int main(int argc, char *argv[]) {
 
         BITMAP * canvas    = create_bitmap(SCREEN_W, SCREEN_H);
 
-        gamelib::allegro::bmp::CCarMapper carMapper(argv[1], width_of_image, width_of_image, 1);
-        //exit_visualizer("leaving for no reason");
-
-        util::CWait wait(50);
-        unsigned cur_tile = 0;
-        const int bg = makecol(0xDE, 0x97, 0x47);
-        while (!key[KEY_ESC]) {
-            rectfill(canvas, 0, 0, WINDOW_W, WINDOW_H, bg);
-            textprintf_ex(canvas,
-                          font,
-                          X_IMAGES,
-                          40,
-                          0,
-                          bg,
-                          "number of tiles: %lu / tile size: (%02d x %02d)",
-                          carMapper.numBmps(),
-                          carMapper[cur_tile]->w,
-                          carMapper[cur_tile]->h);
-            draw_full_image(canvas, carMapper.fullBmp(), X_IMAGES, 60);
-            draw_tile(canvas, carMapper[cur_tile], X_IMAGES, 150);
-            vsync();
-            blit(canvas, screen, 0, 0, 0, 0, SCREEN_W, SCREEN_H);
-            clear_bitmap(canvas);
-
-            wait.reset();
-            if (key[KEY_RIGHT]) {
-                move_right(cur_tile, carMapper.numBmps());
-                wait.wait();
-            } else if (key[KEY_LEFT]) {
-                move_left(cur_tile, carMapper.numBmps());
-                wait.wait();
-            } else if (key[KEY_D]) {
-                move_right(cur_tile, carMapper.numBmps());
-                tools::hold_while_pressed(KEY_D);
-            } else if (key[KEY_A]) {
-                move_left(cur_tile, carMapper.numBmps());
-                tools::hold_while_pressed(KEY_A);
-            }
+        if (type == "car") {
+            gamelib::allegro::bmp::CCarMapper carMapper(argv[1], width_of_image, width_of_image, 1);
+            show(carMapper, canvas);
+        } else if (type == "action") {
+            gamelib::allegro::bmp::CActionMapper actionMapper(argv[1], width_of_image, width_of_image, 1);
+            show(actionMapper, canvas);
         }
 
         return 0;
